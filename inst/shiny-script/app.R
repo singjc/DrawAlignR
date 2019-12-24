c19b87cd671e6bdd1a9b17eb9b9a54d4ce31fed8
Justin Sing <justincsing@gmail.com>
2019-12-24 04:35:39 +0000

    Updates to cacheing mz objects only once, and then sending them to DIAlignR

--- inst/shiny-script/app.R
+++ inst/shiny-script/app.R
@@ -145,20 +145,68 @@ ui <- fluidPage(
       
       #Name of the reference run if performing multiple pairwise alignments. Not required.
       textInput(inputId = "Reference", "Select Reference Run for Alignment", value = "chludwig_K150309_013_SW_0"),
-      
+     
+
     ),
     
+    
     mainPanel(
-      uiOutput("plots")
+      uiOutput("plots"),
+      
+      absolutePanel( id='log-pannel', draggable = TRUE,
+      ## Log
+        fluidRow(
+          
+          # log output
+          textOutput( 'log' )
+        )
+      ),
+      
     )
+ 
+    
   )
 )
 
 server <- function(input, output, session) {
   
+  ## reactive values object to store some re-usable stuff
+  values <- reactiveValues()
+  
   ## Clear Chromatogram File input
   observeEvent( input$resetChromatogramFile, {
     shinyjs::reset('ChromatogramFile')
+    
+    ##*******************************
+    ## Pre-Load mzML Files
+    ##*******************************
+    
+    ## Get filenames from osw files and check if names are consistent between osw and mzML files. ######
+    filenames <- DIAlignR::getRunNames( input$WorkingDirectory, oswMerged=TRUE)
+    runs <- c(input$Reference, gsub('...........$', '', input$ChromatogramFile[,'name']))
+    filenames <- filenames[filenames$runs %in% runs,]
+    tictoc::tic('Pre-Loading mzML Chromatogram Files onto disk')
+    masterMzExperiment <- list()
+    for ( chromatogram_input_index_num in seq(1, length(filenames$runs)) ){
+      run <- rownames(filenames)[ chromatogram_input_index_num ]
+      message(sprintf("Working on run %s", run))
+      ## Get path for current chromatogram file
+      chromatogram_file_i <- input$ChromatogramFile$datapath[[chromatogram_input_index_num]]
+      # Create an mzR object that stores all header information, and use ProteoWizard api to access data from MzML file
+      mz <- mzR::openMSfile(chromatogram_file_i, backend = "pwiz", verbose = T)
+      ## Get table of chromatogram incidces and respective transtion ids
+      chromHead <- mzR::chromatogramHeader(mz)
+      ## Store run id and mz object into master list
+      masterMzExperiment[[run]]$run <- run
+      mzExperiment <- list()
+      mzExperiment$mz <- mz
+      mzExperiment$chromHead <- chromHead
+      masterMzExperiment[[run]]$mzExperiment <- mzExperiment
+    }
+    tictoc::toc()
+    
+    values$masterMzExperiment <- masterMzExperiment
+    
   } )
   
   ## Clear Library File input
@@ -171,8 +219,7 @@ server <- function(input, output, session) {
     shinyjs::reset('OSWFile')
   } )
   
-  ## reactive values object to store some re-usable stuff
-  values <- reactiveValues()
+  
 
   observeEvent( input$LibraryFile, {
     ## Load Librady file into data frame
@@ -218,6 +265,9 @@ server <- function(input, output, session) {
     do.call(tagList, plot_output_list)
   })
   
+  
+    
+  
   #Generate all plots. Max plots set to 10
   
   for (i in 1:10) {
@@ -243,12 +293,50 @@ server <- function(input, output, session) {
         }
         
         if (!(input$Align)){
-          chrom_input <- input$ChromatogramFile[[my_i, 'datapath']]
+          chrom_input <- input$ChromatogramFile$datapath[[my_i]]
           lib_input <- input$LibraryFile
+          osw_input <- input$OSWFile
           peptide <- input$Mod
-          lib <- getPepLibData_(lib_input$datapath, peptide_id = '')
-          g.out <- getXIC(graphic_obj = ggplot(), chromatogram_file = chrom_input,
-                          df_lib = lib, mod = peptide, Isoform_Target_Charge = input$Charge)
+          modification_labels <- regmatches(peptide, gregexpr("\\(.*?\\)", peptide))[[1]]
+          naked_peptide <-  gsub( paste(gsub('\\)','\\\\)',gsub('\\(','\\\\(',modification_labels)), collapse = '|'), '', peptide )
+          # lib <- getPepLibData_(lib_input$datapath, peptide_id = '')
+          # log <- capture.output(
+          # g.out <- getXIC(graphic_obj = ggplot(), chromatogram_file = chrom_input,
+                          # df_lib = lib, mod = peptide, Isoform_Target_Charge = input$Charge)
+          
+          
+          out.plot.h <- mstools::XICMasterPlotFcn_( naked_peptide, 
+                                           peptide,
+                                           chrom_input,  lib_input, osw_input, 
+                                           plotPrecursor=T,
+                                           plotIntersectingDetecting=T,
+                                           plotUniqueDetecting=F,
+                                           plotIdentifying=T,
+                                           plotIdentifying.Unique=T,
+                                           plotIdentifying.Shared=F,
+                                           plotIdentifying.Against=F,
+                                           doFacetZoom=F,
+                                           # FacetFcnCall = NULL,
+                                           doPlot=T,
+                                           # RT_padding=90000,
+                                           Charge_State=input$Charge,
+                                           N_sample = 1,
+                                           # idx_draw_these = c(8),
+                                           # store_plots_subdir = paste('/XIC_plots/TP/U', pool, '/', sep=''),
+                                           # store_plots_subdir = '/Presentation/Figures/IPF_Scoring_With_no_MS 1MS2_precursor_Scoring/6_highest_Concentrations/', 
+                                           # store_plots_subdir = '/Presentation/Figures/Default_IPF_with_5_pkgrprank_transition_scoring/',
+                                           # store_plots_subdir = '/Presentation/Figures/IPF_Scoring_With_no_MS1MS2_precursor_Scoring/', 
+                                           printPlot=T,
+                                           use_top_trans_pep=F,
+                                           show_n_transitions=6,
+                                           show_all_pkgrprnk=F,
+                                           # show_manual_annotation = manual_annotation_coordinates,
+                                           show_legend=T,
+                                           verbosity = 0 )
+          
+          
+          # )
+          # output$log <- renderText( paste(log, collapse = '\n') )
           plotly::ggplotly(g.out$graphic_obj, dynamicTicks = TRUE)
         }
         
@@ -262,21 +350,32 @@ server <- function(input, output, session) {
             
             if ( F ){
               my_i <- 1
+              
               dataPath <- "/media/justincsing/ExtraDrive1/Documents2/Roest_Lab/Github/DrawAlignR/inst/extdata"
               analytes <- "ANSS(UniMod:21)PTTNIDHLK(UniMod:259)_2"
               input <- list()
-              input$ChromatogramFile <- data.frame(name=c("chludwig_K150309_012_SW_1_1.chrom.mzML", "chludwig_K150309_013_SW_0_.chrom.mzML", "chludwig_K150309_010_SW_1_2.chrom.mzML"))
+              input$WorkingDirectory <- "/media/justincsing/ExtraDrive1/Documents2/Roest_Lab/Github/DrawAlignR/inst/extdata"
+              input$Mod <- "ANSS(UniMod:21)PTTNIDHLK(UniMod:259)"
+              input$Charge <- 2
+              input$ChromatogramFile$datapath <- '/media/justincsing/ExtraDrive1/Documents2/Roest_Lab/Github/DrawAlignR/inst/extdata/mzml/chludwig_K150309_013_SW_0.chrom.mzML' 
+              input$ChromatogramFile <- data.frame(name=c("chludwig_K150309_013_SW_0.chrom.mzML", "chludwig_K150309_010_SW_1_2.chrom.mzML"),
+                                                   datapath=c('/media/justincsing/ExtraDrive1/Documents2/Roest_Lab/Github/DrawAlignR/inst/extdata/mzml/chludwig_K150309_013_SW_0.chrom.mzML' ,
+                                                              '/media/justincsing/ExtraDrive1/Documents2/Roest_Lab/Github/DrawAlignR/inst/extdata/mzml/chludwig_K150309_010_SW_1_2.chrom.mzML'),
+                                                              stringsAsFactors=F)
               input$Reference <- "chludwig_K150309_013_SW_0"
+              input$LibraryFile$datapath <- '/media/justincsing/ExtraDrive1/Documents2/Roest_Lab/Github/DrawAlignR/inst/extdata/pqp/psgs_phospho_optimized_decoys.pqp'
             }
             tictoc::tic("DIAlignR Elapsed Time:...")
             dataPath <- input$WorkingDirectory
             analytes <- paste(input$Mod, "_", toString(input$Charge), sep="")
             runs <- c(input$Reference, gsub('...........$', '', input$ChromatogramFile[my_i, 'name']))
-            print(runs)
-            AlignObjOutput <- getAlignObjs(analytes, runs, dataPath)
+            masterMzExperiment <- values$masterMzExperiment
+            print( str(values) )
+            AlignObjOutput <- DIAlignR::getAlignObjs(analytes = analytes, runs = runs, dataPath = dataPath, masterMzExperiment=masterMzExperiment)
+            tictoc::toc()
             k <- plotAlignedAnalytes(AlignObjOutput, DrawAlignR = T, annotatePeak = T)
             plotly::ggplotly(k$prefU, dynamicTicks = TRUE)
-            tictoc::toc()
+            
           }
           
           #If all possible pairwise alignments conducted, plotting the reference run
